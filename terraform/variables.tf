479b239e795fb3d05f50ae590a312b7d109c22be
Rodrigo Santiago <rodrigosantiag@gmail.com>
2025-02-27 18:32:04 -0300

    Add Terraform deployment steps and update variables for DigitalOcean integration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,29 @@
-variable "digital_ocean_token" {
-  description = "The digital ocean API token"
-  type        = string  
-}
-
 variable "ssh_key_id" {
   description = "The SSH key ID to use for the droplet"
-  type        = string  
+  type        = string
 }
 
 variable "github_username" {
   description = "The GitHub username"
-  type        = string    
+  type        = string
 }
 
 variable "github_token" {
   description = "The GitHub personal access token"
-  type        = string      
+  type        = string
 }
 
 variable "do_token" {
-    description = "The digital ocean API token"
-    type        = string  
+  description = "The digital ocean API token"
+  type        = string
 }
 
 variable "secret_key_base" {
-    description = "The secret key base for the Phoenix app"
-    type        = string    
+  description = "The secret key base for the Phoenix app"
+  type        = string
+}
+
+variable "private_key" {
+  description = "The private key to use for the droplet"
+  type        = string
 }
