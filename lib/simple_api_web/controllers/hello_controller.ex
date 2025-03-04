9e94527f0be27ccd0695c07ba99f663f329e2231
Rodrigo Santiago <rodrigosantiag@users.noreply.github.com>
2025-03-04 19:24:06 -0300

    Update hello_controller to return client IP address in response message (#6)

--- lib/simple_api_web/controllers/hello_controller.ex
+++ lib/simple_api_web/controllers/hello_controller.ex
@@ -2,6 +2,18 @@ defmodule SimpleApiWeb.HelloController do
   use SimpleApiWeb, :controller
 
   def index(conn, _params) do
-    json(conn, %{message: "Hello DO droplet #{SimpleApiWeb.Endpoint.host()}!!!"})
+    json(conn, %{message: "Hello DO droplet #{get_ip(conn)}!!!"})
+  end
+
+  defp get_ip(conn) do
+    forwarded_for = List.first(Plug.Conn.get_req_header(conn, "x-forwarded-for"))
+
+    if forwarded_for do
+      String.split(forwarded_for, ",")
+      |> Enum.map(&String.trim/1)
+      |> List.first()
+    else
+      to_string(:inet_parse.ntoa(conn.remote_ip))
+    end
   end
 end
