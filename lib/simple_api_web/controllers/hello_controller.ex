5a526f31eacbf23a18162ede6dbde012ac44768f
Rodrigo Santiago <rodrigosantiag@gmail.com>
2025-03-04 18:30:53 -0300

    Fix endpoint URL method call in hello_controller response

--- lib/simple_api_web/controllers/hello_controller.ex
+++ lib/simple_api_web/controllers/hello_controller.ex
@@ -2,6 +2,6 @@ defmodule SimpleApiWeb.HelloController do
   use SimpleApiWeb, :controller
 
   def index(conn, _params) do
-    json(conn, %{message: "Hello DO droplet #{SimpleApiWeb.Endpoint.url}!!!"})
+    json(conn, %{message: "Hello DO droplet #{SimpleApiWeb.Endpoint.url()}!!!"})
   end
 end
